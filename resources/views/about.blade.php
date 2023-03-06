67bdd1abbb59a30349fa46eacf7e142e2a209d6f
Afeez Aziz <afeezaziz@gmail.com>
2023-03-06 08:13:10 +0800

    321

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,248 @@
-@extends('public.layout')
- 
-@section('title', 'Page Title')
- 
-@section('content')
-    <p>This is my body content.</p>
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;500;700&family=Montserrat:wght@600;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/figma/styles/website_parts/about.css">
+    <title>About Us | VisitKL</title>
+</head>
+<body>
+    <header class="header">
+        <nav class="nav">
+            <div class="nav__container--img">
+                <img src="/figma/src/logos/Visit-KL_Logo_1.png" alt="VisitKL logo"/>
+            </div>
+            <div class="nav__container">
+                <div class="nav__container--wrapper">
+                    <a class="nav__container--wrapper__link home" href="/figma/index.html">HOME</a>
+                    <a class="nav__container--wrapper__link about" href="/">ABOUT US</a>
+                    <a class="nav__container--wrapper__link todo" href="/">THINGS TO DO</a>
+                    <a class="nav__container--wrapper__link products" href="/">PRODUCTS & SERVICES</a>
+                    <a class="nav__container--wrapper__link deals" href="/">DEALS & PACKAGES</a>
+                    <a class="nav__container--wrapper__link gallery" href="/">GALLERY</a>
+                    <a class="nav__container--wrapper__link on" href="/">WHAT'S ON</a>
+                    <a class="nav__container--wrapper__link form" href="/eform">E-FORM</a>
+                    <a class="nav__container--wrapper__link faq" href="/faqs">FAQ</a>
+                </div>
+                <div class="nav__container--wrapper">
+                    <hr class="nav__container--wrapper__line"/>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section class="intro">
+            <div class="intro__head">
+                <img class="intro__head--title" src="/figma/src/logos/about_us_title.svg" alt="about us title"/>
+                <img class="intro__head--info" src="/figma/src/icons/information_cherry-red.svg" alt="information icon"/>
+            </div>
+            <p class="intro__text">
+                This is Kuala Lumpur's official tourism website. It is a one-stop travel information portal that offers you the best recommendations on places to stay, food to try and the fascinating things to do and see - all to experience the best of this vibrant and exciting city. In 2012, KL was the world's 6th most visited city by international tourists. In 2011, KL was awarded "Asia's Leading City Break Destination" by the World Travel Awards. In line with this, the Kuala Lumpur City Hall (KLCH) has initiated several programmes to further spur its growth in the global tourism sector. <br/><br/>
+
+                This website provides links to enable you to make direct bookings with a host of service providers, including tour operators, travel services, accommodations and even venues & attractions. In addition, the KLCH has produced a number of publications that include street directories, maps and brochures that are made available for free at many public places. With all these information conveniently available, we hope that you will enjoy your trip to KL and ultimately, be charmed by its friendly people and captivating attractions.
+            </p>
+        </section>
+
+        <section class="mayor">
+            <div class="mayor__content">
+                <img class="mayor__content--title" src="/figma/src/logos/message_mayor_title.png"/>
+                <p class="mayor__content--text">
+                    Kuala Lumpur is home to me and to 1.8 million other KLites. It is a place where a variety of cultures are all infused into one melting pot to offer a unique experience to visitors. Where else can you savour the meanest street food with one of the world's most iconic structures in the background?   KL is definitely a place where everyday fun and world-class sophistication meet to create a unique destination for travellers, filled with Malaysian charm and friendliness – with greetings of "Selamat Datang" (Welcome) everywhere you go. <br /><br />
+
+                    Kuala Lumpur, the main gateway to Malaysia has a variety of attractions ranging from culture, heritage & places of interest, shopping, entertainment, nature & adventure, luxury travel, sports, business & events, medical & wellness, and education all showcases the city’s contrasts and diversity. This is what makes KL distinctive and the city’s identity was built on its history, people and tradition in the arts and culture. <br /><br />
+
+                    This, I believe is why KL has been ranked as a Top 10 City Destination in the World by Euromonitor International for more than 5 years now. The contrasting history and heritage, from the early days of the small tin mining trading town to the metropolis city it is today yet retaining her fundamental identity, will excite, surprise and entice you to discover more. <br /><br />
+
+                    KL is well recognised as a shopper's paradise. I invite you to take a stroll within KL's deluxe shopping centres, now linked by comfortable walkways, and vibrant night bazaars and be spoilt for choices. In our city, there's always a time and place to shop. In 2013, KL was voted as the world's fourth best place to shop by CNN Travel. This is a feat I am truly proud of as I see this as a sign of KL opening up to smart and discerning travelers.<br /><br />
+
+                    I assure you that you will enjoy your time savouring the amazingly diverse culinary options and exploring what the city has to offer. KL's unique diversity - the Malays, Chinese, Indians and other ethnicities - have all injected their own unique influences to the city's art, language, architecture and night scenes. The city is full of flavours, colours and experiences waiting for you to explore.<br /><br />
+
+                    As I end this, I wish you a wonderful time in KL, and hope that you'll be greeted with a warm "Selamat Datang" as you enter our city of contrasts and diversity.<br /><br />
+
+                    Enjoy your holiday in KL !
+                </p>
+                <button class="mayor__content--button">Click here to play the Mayor’s video</button>
+            </div>
+            <div class="mayor__image">
+                <img class="mayor__image--picture" src="/figma/src/images/mayor_image.png" alt="mayor of kuala lumpur"/>
+                <h4 class="mayor__image--title">Datuk Hj. Mahadi bin Che Ngah</h4>
+                <p class="mayor__image--text">Mayor of Kuala Lumpur</p>
+            </div>
+        </section>
+
+        <section class="logo">
+            <div class="logo__image">
+                <img class="logo__image--picture" src="/figma/src/logos/VisitKL_logo_photo.png" alt="visitKL logo" />
+            </div>
+            <div class="logo__content">
+                <img class="logo__content--title" src="/figma/src/logos/visitKL_logo_title.png" alt="visitkl logo title" />
+                <p class="logo__content--text">
+                    The Visit KL Logo was designed as an icon that represents the colourful experiences that are unique to Malaysia's capital city, Kuala Lumpur. It is part a larger branding exercise that is being implemented to create a distinct image that will be easily recognised all over the world.<br/><br/>
+
+                    The Visit KL Logo was officially launched on the 25 of June 2008, and has been used in a multitude of applications since. Developed in three variations, the logo is used as a seal of endorsement from Kuala Lumpur City Hall for a variety of sanctioned events that are held within the city.
+                </p>
+            </div>
+        </section>
+
+        <section class="rationale">
+            <h2 class="rationale__title">Logo <br/>Rationale</h2>
+            <div class="rationale__content">
+                <p class="rationale__content--text">
+                    <b>The Objectives of the Logo</b> <br/><br/>
+
+                    The main objectives of the logo are as follows:<br/>
+                    The logo represents the endorsement of Kuala Lumpur City Hall in all sanctioned events in the city.<br/><br/>
+
+                    This logo will form part of the new campaign to brand KL.<br/>
+                    The logo will eventually become a recognisable icon that represents the KL brand to the world.
+                </p>
+            </div>
+        </section>
+
+        <section class="info">
+            <p class="info__text">
+                <b>The Look & Feel of the Logo</b><br/>
+                The logo was purposely designed to create a trendy, modern and very inviting feel. The colours used are bright and vibrant and the graphic elements are applied in an almost playful manner to reflect the feeling of excitement and fun that the city has to offer. The design approach is also very contemporary that will reflect the image of the city as a very modern city that has not undermined its rich cultural heritage.<br/><br/>
+
+                <b>The Intended Use of the Logo</b><br/>
+                The logo will be used in all printed communication items produced by KL City Hall and the KL Tourism Action Council, that are used to promote and brand KL. The logo will also be used as an endorsement on all communication items produced to promote specific events that are sanctioned by KL City Hall.<br/><br/>
+
+                <b>Components of the Logo</b><br/>
+                The logo is made up of two main components: the combination of graphic elements that form the cityscape and the text below it. The cityscape is made up of six graphic elements:
+            </p>
+        </section>
+
+        <section class="parts">
+            <div class="parts__container">
+                <div class="parts__container--wrapper">
+                    <article class="parts__container--wrapper__item">
+                        <img src="/figma/src/images/petronas_square.png" alt="petronas towers" />
+                    </article>
+                    <article class="parts__container--wrapper__item">
+                        <img src="/figma/src/images/sultan_square.png" alt="sultan building" />
+                    </article>
+                    <article class="parts__container--wrapper__item">
+                        <img src="/figma/src/images/kl_tower_square.png" alt="kl tower" />
+                    </article>
+                    <article class="parts__container--wrapper__item">
+                        <img src="/figma/src/images/sun_square.png" alt="the sun" />
+                    </article>
+                </div>
+                <div class="parts__container--wrapper">
+                    <article class="parts__container--wrapper__item">
+                        <img src="/figma/src/images/curved_square.png" alt="curved base" />
+                    </article>
+                    <article class="parts__container--wrapper__item">
+                        <img src="/figma/src/images/flowers_square.png" alt="the flowers" />
+                    </article>
+                    <article class="parts__container--wrapper__item">
+                        <img src="/figma/src/images/visitKL_square.png" alt="visitKL" />
+                    </article>
+                </div>
+            </div>
+        </section>
+
+        <section class="variants">
+            <p class="variants__text">
+                <b>Colour Schemes and Variants of The Logo</b><br/>
+                The main logo colour scheme is red for the Graphic Elements and blue for the Text Element. The colour variations are created to dynamic options for the application of the logo.
+            </p>
+            <div class="variants__wrapper">
+                <article class="variants__wrapper--item">
+                    <img src="/figma/src/logos/visitKL_red.png" alt="red"/>
+                    <div class="variants__wrapper--item__content">
+                        <h3>The Red</h3>
+                        <p>
+                            It is the main colour used in the DBKL official emblem.<br/>
+                            It is a cheerful and vibrant colour depicting the experience visitors are promised.<br/>
+                            Red is eye-catching and distinctive.
+                        </p>
+                    </div>
+                </article>
+                <article class="variants__wrapper--item">
+                    <img src="/figma/src/logos/visitKL_blue.png" alt="blue"/>
+                    <div class="variants__wrapper--item__content">
+                        <h3>The Blue</h3>
+                        <p>
+                            It is the second most-used colour in the DBKL official emblem.<br/>
+                            It contrasts well with the red of the graphic icons.<br/>
+                            The Blue and Red reverse option is to be used for all sporting events.
+                        </p>
+                    </div>
+                </article>
+                <article class="variants__wrapper--item">
+                    <img src="/figma/src/logos/visitKL_gold.png" alt="gold"/>
+                    <div class="variants__wrapper--item__content">
+                        <h3>The Gold and Grey</h3>
+                        <p>
+                            The Gold and Grey application is to be used for all cultural events.
+                        </p>
+                    </div>
+                </article>
+            </div>
+        </section>
+
+        <section class="links">
+            <div class="links__title">
+                <h2 class="links__title--text">Quick Links</h2>
+                <hr class="links__title--line"/>
+            </div>
+            <div class="links__container">
+                <a class="links__container--item" href="https://www.kwp.gov.my/" target="_blank" rel="noopener noreferrer">
+                    <img src="/figma/src/logos/link_1.png" alt="kwp"/>
+                </a>
+                <a class="links__container--item" href="https://www.motac.gov.my/" target="_blank" rel="noopener noreferrer">
+                    <img src="/figma/src/logos/link_2.png" alt="motac"/>
+                </a>
+                <a class="links__container--item" href="https://www.dbkl.gov.my/" target="_blank" rel="noopener noreferrer">
+                    <img src="/figma/src/logos/link_3.png" alt="dbkl"/>
+                </a>
+                <a class="links__container--item" href="https://www.malaysia.travel/" target="_blank" rel="noopener noreferrer">
+                    <img src="/figma/src/logos/link_4.png" alt="tourism malaysia"/>
+                </a>
+                <a class="links__container--item" href="https://www.matic.gov.my/en/" target="_blank" rel="noopener noreferrer">
+                    <img src="/figma/src/logos/link_5.png" alt="malaysia turism center"/>
+                </a>
+                <a class="links__container--item" href="https://www.mm2h.com/" target="_blank" rel="noopener noreferrer">
+                    <img src="/figma/src/logos/link_6.png" alt="malaysia my second home"/>
+                </a>
+                <a class="links__container--item" href="https://asean.org/" target="_blank" rel="noopener noreferrer">
+                    <img src="/figma/src/logos/link_7.png" alt="southeast asia"/>
+                </a>
+                <a class="links__container--item" href="https://www.matrade.gov.my/en/" target="_blank" rel="noopener noreferrer">
+                    <img src="/figma/src/logos/link_8.png" alt="matrade"/>
+                </a>
+                <a class="links__container--item" href="https://www.malaysiaairports.com.my/" target="_blank" rel="noopener noreferrer">
+                    <img src="/figma/src/logos/link_9.png" alt="malaysia airports"/>
+                </a>
+                <a class="links__container--item" href="https://www.malaysiaairports.com.my/" target="_blank" rel="noopener noreferrer">
+                    <img src="/figma/src/logos/link_10.png" alt="klia next gen hub"/>
+                </a>
+                <a class="links__container--item" href="https://www.matta.org.my/" target="_blank" rel="noopener noreferrer">
+                    <img src="/figma/src/logos/link_11.png" alt="matta"/>
+                </a>
+                <a class="links__container--item" href="https://www.artgallery.gov.my/en/homepage/" target="_blank" rel="noopener noreferrer">
+                    <img src="/figma/src/logos/link_12.png" alt="balai seni lukis negara"/>
+                </a>
+            </div>
+        </section>
+    </main>
+    <footer id="footer" class="footer">
+        <div class="footer__containerLeft">
+            <p class="footer__containerLeft--textLeft">Copyright      
+                <img class="footer__containerLeft--image" src="/figma/src/icons/copyright_white.svg" alt="copyright icon"/> 2022 <br/>
+                All Right Reserved - <a href="https://www.tourism.gov.my/" target="_blank" rel="noopener noreferrer">Tourism Unit, Kuala Lumpur City Hall</a>
+            </p>
+        </div>
+        <div class="footer__containerRight">
+            <p class="footer__containerRight--textRight">Total Visitor : 9999999 <br/>
+                Best viewd in Google Chrome with 1280 x 720 resolution
+            </p>
+        </div>
+    </footer>
+</body>
+</html>
